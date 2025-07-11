7dad1acd4a23ca4bf717776d0db64db1e6f2b181
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-11 19:27:29 +0300

    L-481, 482. Get newsId with useRouter. Create a list of dummy articles and navigate to them with Link

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,9 @@
+import { useRouter } from 'next/router';
+
 function DetailPage() {
+  const router = useRouter();
+  const newsId = router.query.newsId
+
   return (
     <h1>Detail page</h1>
   );
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,14 @@
+import Link from "next/link";
+
 function NewsPage() {
   return (
-    <h1>News page</h1>
+    <>
+      <h1>News page</h1>
+      <ul>
+        <li><Link href="/news/nextJsArticle">NextJS is a Great Framework</Link></li>
+        <li><Link href="/news/javaArticle">Java is the Best Framework</Link></li>
+      </ul>
+    </>
   );
 }
 
